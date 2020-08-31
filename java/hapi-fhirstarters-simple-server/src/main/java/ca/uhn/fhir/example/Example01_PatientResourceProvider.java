4dd3daa1e656d5800191a55cc9bca933474ea3b5
TonyToscana <palomoborja98@gmail.com>
2020-08-31 20:30:22 +0200

    Add needed dependencies

--- java/hapi-fhirstarters-simple-server/src/main/java/ca/uhn/fhir/example/Example01_PatientResourceProvider.java
+++ java/hapi-fhirstarters-simple-server/src/main/java/ca/uhn/fhir/example/Example01_PatientResourceProvider.java
@@ -1,47 +1,125 @@
 package ca.uhn.fhir.example;
 
+import ca.uhn.fhir.context.FhirContext;
+import ca.uhn.fhir.parser.IParser;
+import ca.uhn.fhir.rest.annotation.Create;
 import ca.uhn.fhir.rest.annotation.IdParam;
 import ca.uhn.fhir.rest.annotation.Read;
+import ca.uhn.fhir.rest.annotation.ResourceParam;
+import ca.uhn.fhir.rest.api.MethodOutcome;
 import ca.uhn.fhir.rest.server.IResourceProvider;
-import ca.uhn.fhir.rest.server.exceptions.ResourceNotFoundException;
 import org.hl7.fhir.r4.model.IdType;
-import org.hl7.fhir.r4.model.Patient;
 import org.hl7.fhir.instance.model.api.IBaseResource;
+import org.json.JSONObject;
 
-import java.util.HashMap;
-import java.util.Map;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.MediaType;
 
-public class Example01_PatientResourceProvider implements IResourceProvider {
+import java.lang.reflect.ParameterizedType;
 
-   private Map<String, Patient> myPatients = new HashMap<String, Patient>();
+
+public abstract class Example01_PatientResourceProvider<T extends IBaseResource> implements IResourceProvider {
+
+   private final String ORION_BASE = "http://localhost:1026/v2/entities";
+   private FhirContext ctx = FhirContext.forR4();
+
+   private final String TYPE = "type";
+   private final String RESOURCE_TYPE = "resourceType";
+
+   private Class<T> inferedClass;
 
    /**
     * Constructor
     */
    public Example01_PatientResourceProvider() {
-      Patient pat1 = new Patient();
-      pat1.setId("1");
-      pat1.addIdentifier().setSystem("http://acme.com/MRNs").setValue("7000135");
-      pat1.addName().setFamily("Simpson").addGiven("Homer").addGiven("J");
-      myPatients.put("1", pat1);
+
    }
 
    @Override
    public Class<? extends IBaseResource> getResourceType() {
-      return Patient.class;
+      if(inferedClass == null){
+         inferedClass = (Class<T>) ((ParameterizedType) getClass().getGenericSuperclass()).getActualTypeArguments()[0];
+      }
+
+      return inferedClass;
    }
 
    /**
     * Simple implementation of the "read" method
     */
    @Read()
-   public Patient read(@IdParam IdType theId) {
-      Patient retVal = myPatients.get(theId.getIdPart());
-      if (retVal == null) {
-         throw new ResourceNotFoundException(theId);
-      }
+   public IBaseResource read(@IdParam IdType theId) {
+      Client client = ClientBuilder.newClient();
+      final String URL = ORION_BASE + "/" + theId.getIdPart() + "?options=keyValues&type=" + theId.getResourceType();
+      Response response = client.target(URL)
+         .request(MediaType.APPLICATION_JSON_TYPE)
+         .get();
+
+      String entityJsonString = response.readEntity(String.class);
+
+      IParser parser = ctx.newJsonParser();
+
+      //Patient retVal = myPatients.get(theId.getIdPart());
+      //if (retVal == null) {
+      //   throw new ResourceNotFoundException(theId);
+      //}
+      return parser.parseResource(getResourceType(), entityToResource(entityJsonString));
+   }
+
+   @Create()
+   public MethodOutcome create(@ResourceParam IBaseResource patient) {
+      patient.setId(getNextId(patient.fhirType()));
+
+      IParser parser = ctx.newJsonParser();
+      String resourceJsonString = parser.encodeResourceToString(patient);
+
+      Client client = ClientBuilder.newClient();
+      final String URL = ORION_BASE + "?options=keyValues";
+      Response response = client.target(URL)
+         .request(MediaType.APPLICATION_JSON_TYPE)
+         .post(Entity.json(resourceToEntity(resourceJsonString)));
+
+      MethodOutcome retVal = new MethodOutcome();
+
+      retVal.setId(patient.getIdElement());
+
       return retVal;
    }
 
+   private long getEntityCount(String entityType) {
+      Client client = ClientBuilder.newClient();
+      String url = ORION_BASE + "?type=" + entityType + "&options=count&limit=1";
+      Response response = client.target(url)
+         .request(MediaType.APPLICATION_JSON_TYPE)
+         .get();
 
+      return Long.parseLong(response.getHeaderString("Fiware-Total-Count"));
+   }
+
+   private String getNextId(String entityType) {
+      return Long.toString(getEntityCount(entityType) + 1);
+   }
+
+   private String resourceToEntity(String resource) {
+      JSONObject entityJsonArray = new JSONObject(resource);
+
+      entityJsonArray.putOpt(TYPE, entityJsonArray.opt(RESOURCE_TYPE));
+      if(entityJsonArray.has(TYPE))
+         entityJsonArray.remove(RESOURCE_TYPE);
+
+      return entityJsonArray.toString();
+   }
+
+   private String entityToResource(String entity) {
+      JSONObject entityJsonArray = new JSONObject(entity);
+
+      entityJsonArray.putOpt(RESOURCE_TYPE, entityJsonArray.opt(TYPE));
+      if(entityJsonArray.has(RESOURCE_TYPE))
+         entityJsonArray.remove(TYPE);
+
+      return entityJsonArray.toString();
+   }
 }
