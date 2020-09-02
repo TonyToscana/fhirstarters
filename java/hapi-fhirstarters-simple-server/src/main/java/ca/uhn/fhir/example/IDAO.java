636c6843890c97e82a3c02f59d3efab771f0f059
TonyToscana <palomoborja98@gmail.com>
2020-09-03 00:49:15 +0200

    Update IDAO

--- java/hapi-fhirstarters-simple-server/src/main/java/ca/uhn/fhir/example/IDAO.java
+++ java/hapi-fhirstarters-simple-server/src/main/java/ca/uhn/fhir/example/IDAO.java
@@ -3,6 +3,8 @@ package ca.uhn.fhir.example;
 public interface IDAO {
    String read(String id, String type);
    void create(String resource);
+   void update(String id, String type, String resource);
    void delete(String id, String type);
    long retrieveEntityCount(String type);
+   String getNextId(String entityType);
 }
